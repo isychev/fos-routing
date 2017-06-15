7414c3913dacf42185c51ee1e0491878e70eaa0c
i.sychev <i.sychev@rambler-co.ru>
2017-06-15 12:58:08 +0300

    init main files

---
+++ src/routing.js
@@ -0,0 +1,172 @@
+class Routing {
+  constructor() {
+    this.contextRoutung = { base_url: '', prefix: '', host: '', scheme: '' };
+  }
+  setRoutes = routes => {
+    this.routesRouting = routes || [];
+  };
+  getRoutes = () => this.routesRouting;
+  setBaseUrl = baseUrl => {
+    this.contextRoutung.base_url = baseUrl;
+  };
+  getBaseUrl = () => this.contextRoutung.base_url;
+  setPrefix = prefix => {
+    this.contextRoutung.prefix = prefix;
+  };
+  setScheme = scheme => {
+    this.contextRoutung.scheme = scheme;
+  };
+  getScheme = () => this.contextRoutung.scheme;
+  setHost = host => {
+    this.contextRoutung.host = host;
+  };
+  getHost = () => this.contextRoutung.host;
+
+  buildQueryParams = (prefix, params, add) => {
+    const self = this;
+    const rbracket = new RegExp(/\[]$/);
+    if (params instanceof Array) {
+      params.forEach((val, i) => {
+        if (rbracket.test(prefix)) {
+          add(prefix, val);
+        } else {
+          self.buildQueryParams(
+            `${prefix}[${typeof val === 'object' ? i : ''}]`,
+            val,
+            add
+          );
+        }
+      });
+    } else if (typeof params === 'object') {
+      Object.keys(params).forEach(name =>
+        this.buildQueryParams(`${prefix}[${name}]`, params[name], add)
+      );
+    } else {
+      add(prefix, params);
+    }
+  };
+  getRoute = name => {
+    const prefixedName = this.contextRoutung.prefix + name;
+    if (!this.routesRouting[prefixedName]) {
+      if (!this.routesRouting[name]) {
+        throw new Error(`The route "${name}" does not exist.`);
+      }
+    } else {
+      return this.routesRouting[prefixedName];
+    }
+    return this.routesRouting[name];
+  };
+  generate = (name, optParams, absolute) => {
+    const route = this.getRoute(name);
+    const params = optParams || {};
+    const unusedParams = Object.assign({}, params);
+    const schemaVar = '_scheme';
+    let url = '';
+    let optional = true;
+    let host = '';
+    (route.tokens || []).forEach(token => {
+      if (token[0] === 'text') {
+        url = token[1] + url;
+        optional = false;
+        return;
+      }
+      if (token[0] === 'variable') {
+        const hasDefault = (route.defaults || {})[token[3]];
+        if (
+          optional === false ||
+          !hasDefault ||
+          ((params || {})[token[3]] &&
+            params[token[3]] !== route.defaults[token[3]])
+        ) {
+          let value;
+          if ((params || {})[token[3]]) {
+            value = params[token[3]];
+            delete unusedParams[token[3]];
+          } else if (hasDefault) {
+            value = route.defaults[token[3]];
+          } else if (optional) {
+            return;
+          } else {
+            throw new Error(
+              `The route "${name}" requires the parameter "${token[3]}".`
+            );
+          }
+          const empty = value === true || value === false || value === '';
+          if (!empty || !optional) {
+            let encodedValue = encodeURIComponent(value).replace(/%2F/g, '/');
+            if (encodedValue === 'null' && value === null) {
+              encodedValue = '';
+            }
+            url = token[1] + encodedValue + url;
+          }
+          optional = false;
+        } else if (hasDefault) {
+          delete unusedParams[token[3]];
+        }
+        return;
+      }
+      throw new Error(`The token type "${token[0]}" is not supported.`);
+    });
+    if (url === '') {
+      url = '/';
+    }
+    (route.hosttokens || []).forEach(token => {
+      let value;
+      if (token[0] === 'text') {
+        host = token[1] + host;
+        return;
+      }
+      if (token[0] === 'variable') {
+        if ((params || {})[token[3]]) {
+          value = params[token[3]];
+          delete unusedParams[token[3]];
+        } else if (route.defaults[token[3]]) {
+          value = route.defaults[token[3]];
+        }
+        host = token[1] + value + host;
+      }
+    });
+    url = this.contextRoutung.base_url + url;
+    if (
+      route.requirements[schemaVar] &&
+      this.getScheme() !== route.requirements[schemaVar]
+    ) {
+      url = `${route.requirements[schemaVar]}://${host ||
+        this.getHost()}${url}`;
+    } else if (host && this.getHost() !== host) {
+      url = `${this.getScheme()}://${host}${url}`;
+    } else if (absolute === true) {
+      url = `${this.getScheme()}://${this.getHost()}${url}`;
+    }
+    if (Object.keys(unusedParams).length > 0) {
+      const queryParams = [];
+      const add = (key, value) => {
+        let newValue = value;
+        newValue = typeof newValue === 'function' ? newValue() : newValue;
+        newValue = newValue === null ? '' : newValue;
+        queryParams.push(
+          `${encodeURIComponent(key)}=${encodeURIComponent(newValue)}`
+        );
+      };
+      Object.keys(unusedParams).forEach(prefix =>
+        this.buildQueryParams(prefix, unusedParams[prefix], add)
+      );
+      url = `${url}?${queryParams.join('&').replace(/%20/g, '+')}`;
+    }
+    return url;
+  };
+
+  setData = data => {
+    this.setBaseUrl(data.base_url);
+    this.setRoutes(data.routes);
+    if ('prefix' in data) {
+      this.setPrefix(data.prefix);
+    }
+    this.setHost(data.host);
+    this.setScheme(data.scheme);
+  };
+}
+module.export = new Routing();
+
+
+
